214513e3a6a0e5547a7a1b80428841c72db71036
jeevanagi99 <89789326+jeevanagi99@users.noreply.github.com>
2021-09-08 18:53:40 +0530

    Add files via upload

---
+++ NaturalNumbers.java
@@ -0,0 +1,15 @@
+package com.X_workz.selectionStatments;
+
+public class NaturalNumbers {
+
+	public static void main(String[] args) {
+		int num=100,i=1,sum=0;
+		while(i<=num) {
+			sum=sum+i;
+			i++;}
+		System.out.println("sum of 100 natural nos are:"+sum);
+		}
+		
+
+	}
+
